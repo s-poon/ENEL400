8b9198bb1edaa0d0e1b5fa5f2f548f221089c6e7
s-poon <stevenpoon12.03@gmail.com>
2022-03-25 17:51:47 -0600

    works just needs to be able to read gcode

--- Plotter.py
+++ Plotter.py
@@ -8,11 +8,21 @@ import sys
 GPIO.setmode(GPIO.BCM)
 
 motorX = StepperMotor(21, 20)
+motorY = StepperMotor(19, 26)
+switchX  = 25
+switchY = 24
 
+
+# 0.04mm per step
 
 try:
     while True:
-        motorX.move(0, 20)
+        # motorX.move(0, 200)
+        MotorControl.motorStep(motorX, 200, motorY, 0)
+        MotorControl.motorStep(motorX, 0, motorY, 200)
+        MotorControl.motorStep(motorX, -200, motorY, 0)
+        MotorControl.motorStep(motorX, 0, motorY, -200)
+        time.sleep(2)
         print('motor is working')
 
 except KeyboardInterrupt:
